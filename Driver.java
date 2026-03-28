fdc6227660be46c9a7487fe894ffc8a85f51364b
AlmostHome-01 <jlfisher2001@yahoo.com>
2026-03-28 11:25:02 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,10 +1,34 @@
-package C4;
+import java.util.*;
 
 public class Driver {
 
+	private String[][] board = new String[7][7];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		int c1, c2, c3, c4, c5, c6, c7 = 0;
+		int playerChoice;
+		
+		System.out.println("Welcome to Connect 4!");
+		while (isFour() == false)
+		System.out.print("Chose which column to play your piece Player 1 (1-7): ");
+		playerChoice = scnr.nextInt();
 
+		//board[][playerChoice] = "P1";
+
+	}
+
+    public static String columnVar(int playerChoice){
+        if (playerChoice==0){
+
+        }
+        return "";
+    }
+	
+	public static boolean isFour() {
+		return false;
 	}
 
 }
